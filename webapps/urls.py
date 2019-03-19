0fdc7a66b5a4cee6541cb829786b1558107f92de
Jeffrey Eppinger <eppinger@cmu.edu>
2019-03-19 11:19:50 -0400

    Turned on Django admin to demo this feature for class.

--- webapps/urls.py
+++ webapps/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from welcome import views
 
 urlpatterns = [
-    path('',  views.home_action),
-    path('welcome/',  include('welcome.urls')),
-    path('addrbook2/',  include('addrbook2.urls')),
-    path('todolist2/',  include('todolist2.urls')),
+    path('', views.home_action),
+    path('admin/', admin.site.urls),
+    path('welcome/', include('welcome.urls')),
+    path('addrbook2/', include('addrbook2.urls')),
+    path('todolist2/', include('todolist2.urls')),
 ]
